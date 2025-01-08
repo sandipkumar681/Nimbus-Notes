f573db8e5430ba05f18945afd57809645231ef05
SANDIP KUMAR BEHERA <163564265+sandipkumar681@users.noreply.github.com>
2025-01-08 12:49:26 +0530

    Update LoadingBarState.js

--- frontend/src/context/loadingBar/LoadingBarState.js
+++ frontend/src/context/loadingBar/LoadingBarState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import loadingBarContext from "./loadingBarContext";
 
 const LoadingBarState = (props) => {
-  const [progress, setProgress] = useState(1);
+  const [progress, setProgress] = useState(0);
 
   return (
     <loadingBarContext.Provider value={{ progress, setProgress }}>
